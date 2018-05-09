45991e7822a256144c3aa6ed3ecfc65e2c1ff974
Sven van Hal <sven@svenvanhal.nl>
2018-05-09 11:46:12 +0200

    Setup NUnit (#26)
    * Add NUnit, add example test.
    
    * Fix class reference in project.
    
    * Change example test name.

---
+++ timetable/Application.Tests/ApplicationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Application.Tests
+{
+    class ApplicationTest
+    {
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.IsNotNull("Test");
+        }
+
+    }
+}
