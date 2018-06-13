a496426eac654f4c8b40e5c3657f07ab6745de1f
Sven van Hal <sven@svenvanhal.nl>
2018-06-13 11:17:37 +0200

    Add test for Timetable resource. (#123)

---
+++ Timetabling.Tests/Resources/TimetableTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Timetabling.Resources;
+
+namespace Timetabling.Tests.Resources
+{
+
+    class TimetableTest
+    {
+
+        [Test]
+        public void TimetableNotPartialTest()
+        {
+
+            var tt = new Timetable
+            {
+                PlacedActivities = 1,
+                Activities = new List<Timetable.TimetableActivity>
+                {
+                    { new Timetable.TimetableActivity() }
+                }
+            };
+
+            Assert.IsFalse(tt.IsPartial);
+        }
+
+        [Test]
+        public void TimetablePartialTest()
+        {
+
+            var tt = new Timetable
+            {
+                PlacedActivities = 0,
+                Activities = new List<Timetable.TimetableActivity>
+                {
+                    { new Timetable.TimetableActivity() }
+                }
+            };
+
+            Assert.IsTrue(tt.IsPartial);
+        }
+
+        [Test]
+        public void TimetablePartialNullTest()
+        {
+
+            var tt = new Timetable
+            {
+                Activities = null
+            };
+
+            Assert.DoesNotThrow(() => {
+                var _ = tt.IsPartial;
+            });
+        }
+
+    }
+}
